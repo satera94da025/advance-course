0c5626e610d474eb520beb10af74b4f200cc5c36
Dovydov Alexandr <AADovydov@sberbank.ru>
2023-01-31 16:37:53 +0300

    Добавил роутинг, разбиение бандла на чанки

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,6 +5,7 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     const {port} = options
     return {
         port,
-        open: true
+        open: true,
+        historyApiFallback: true
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 const App = () => {
     return (
         <div className="app">
-            TEST
+            <BrowserRouter>
+                <Link to={'/'}>Главная</Link>
+                <Link to={'/about'}>О сайте</Link>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Routes>
+                        <Route path={'/about'} element={<AboutPageAsync />}/>
+                        <Route path={'/'} element={<MainPageAsync />}/>
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+
         </div>
     );
 };
