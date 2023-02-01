b9f6947e4d3e5fa7185f3331e65b92c1175cd29f
Dovydov Alexandr <AADovydov@sberbank.ru>
2023-02-01 13:55:31 +0300

    Добавил хук для изменения темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React, {Suspense} from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
+import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-const App = () => {
-    return (
-        <div className="app">
-            <BrowserRouter>
-                <Link to={'/'}>Главная</Link>
-                <Link to={'/about'}>О сайте</Link>
-                <Suspense fallback={<p>Loading...</p>}>
-                    <Routes>
-                        <Route path={'/about'} element={<AboutPageAsync />}/>
-                        <Route path={'/'} element={<MainPageAsync />}/>
-                    </Routes>
-                </Suspense>
-            </BrowserRouter>
+import './styles/index.scss'
+import {useTheme} from "./theme/useTheme";
 
+
+
+const App = () => {
+
+   const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
