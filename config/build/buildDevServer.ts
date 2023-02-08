2a3df4526fccd3da40febbc50f9c6a956678856a
Dovydov Alexandr <AADovydov@sberbank.ru>
2023-02-08 22:51:43 +0300

    Добавил плагины для автообновления приложения

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,6 +6,7 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port,
         open: true,
-        historyApiFallback: true
+        historyApiFallback: true,
+        hot: true
     }
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,14 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 import {DefinePlugin, ProgressPlugin, WebpackPluginInstance} from "webpack";
 import {BuildOptions} from "./types/config";
+
 const progressInfo = (percentage: number, message: string, ...args: string[]) => console.info(percentage, message, ...args);
+
 export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -15,6 +19,10 @@ export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstanc
         }),
         new DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
     ]
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin())
+    }
+    return plugins
 }
