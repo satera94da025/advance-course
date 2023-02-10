6119e24529266da1242f6b1c7acd31873b2427d3
Dovydov Alexandr <AADovydov@sberbank.ru>
2023-02-10 15:26:07 +0300

    Добавил и настроил jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,7 +3,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
+    ...Object.entries(mods)
       .filter(([_, value]) => Boolean(value))
       .map(([className]) => className),
   ].join(' ');
