0754c6a47d5518fcf9d0f3b0007ce46470ae76cb
Dovydov Alexandr <AADovydov@sberbank.ru>
2023-02-18 20:53:42 +0300

    Добавил и настроил Loki

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-import ThemeSwitcher from 'widgets/ThemeSwitcher/ui/ThemeSwitcher';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher/ui/ThemeSwitcher';
 
 import cls from './Sidebar.module.scss';
 
